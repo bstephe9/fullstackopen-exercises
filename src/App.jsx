141a415c073c2907e996007503e603ceb577a1b6
bstephe9 <bstephenson9807@gmail.com>
2025-06-02 13:59:55 -0400

    Set initial app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
   return (
     <>
-
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <br />
+      {right}
+      <button onClick={() => setRight(right + 1)}>right</button>
     </>
   )
 }
