d167a008352c3f284fa060031375b1380c484451
bstephe9 <bstephenson9807@gmail.com>
2025-06-02 13:49:43 -0400

    1.c

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-import './App.css'
+import { useState } from 'react'
+import Display from './components/Display'
+import Button from './components/Button'
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
     <>
-
+      <Display counter={counter}></Display>
+      <Button
+        onClick={increaseByOne}
+        text="plus"
+      />
+      <Button
+        onClick={decreaseByOne}
+        text="minus"
+      />
+      <Button
+        onClick={setToZero}
+        text="reset"
+      />
     </>
   )
 }
