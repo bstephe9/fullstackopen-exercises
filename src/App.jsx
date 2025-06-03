24ea1432fcaafbea2bf2ad09cd2643b6714b5bb6
bstephe9 <bstephenson9807@gmail.com>
2025-06-03 00:09:37 -0400

    1.d

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { useState } from 'react'
+import History from './components/History'
 import './App.css'
 
 function App() {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+  }
 
   return (
     <>
       {left}
-      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={handleLeftClick}>left</button>
       <br />
       {right}
-      <button onClick={() => setRight(right + 1)}>right</button>
+      <button onClick={handleRightClick}>right</button>
+      <br />
+      <History allClicks={allClicks}></History>
     </>
   )
 }
