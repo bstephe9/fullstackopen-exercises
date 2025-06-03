1469c146adaa927a74cca4a599a9aa4a250b2673
bstephe9 <bstephenson9807@gmail.com>
2025-06-03 12:36:14 -0400

    Step 4: Conditionally render feedback

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,16 +1,20 @@
-function Statistics(props) {
-  let total = props.good + props.neutral + props.bad
-  let average = (props.good - props.bad) / total
-  let positive = (props.good / total) * 100
+function Statistics({ good, neutral, bad }) {
+  let total = good + neutral + bad
+  let average = (good - bad) / total
+  let positive = (good / total) * 100
+
+  if (good + neutral + bad == 0) {
+    return <div>No feedback given</div>
+  }
 
   return (
     <>
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
       <div>all {total}</div>
-      <div>average {average ? average : 0}</div>
-      <div>positive {positive ? positive : 0} %</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </>
   )
 }
