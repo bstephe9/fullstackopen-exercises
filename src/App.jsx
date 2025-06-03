2414e583a538816e78e243516578f6444a6d78ff
bstephe9 <bstephenson9807@gmail.com>
2025-06-03 01:31:27 -0400

    Step 2: Add additional statistics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ function App() {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  let total = good + neutral + bad
+  let average = (good - bad) / total
+  let positive = (good / total) * 100
+
   return (
     <>
       <h1>give feedback</h1>
@@ -17,6 +21,9 @@ function App() {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average ? average : 0}</div>
+      <div>positive {positive ? positive : 0} %</div>
     </>
   )
 }
