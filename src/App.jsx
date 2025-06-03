afe987121588f08bdb3ac9196698c986953e74f6
bstephe9 <bstephenson9807@gmail.com>
2025-06-03 01:19:36 -0400

    Step 1: Display and set feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
 import './App.css'
+import Button from './Button'
 
 function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
     <>
-
+      <h1>give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good"></Button>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"></Button>
+      <Button onClick={() => setBad(bad + 1)} text="bad"></Button>
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
     </>
   )
 }
